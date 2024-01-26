c2333aa28a694e028466e85e75ff8246d13ea34e
EliteUn17y <68102994+EliteUn17y@users.noreply.github.com>
2024-01-26 18:28:02 +0000

    Change XOR to plain

---
+++ src/uv.config.js
@@ -0,0 +1,12 @@
+/*global Ultraviolet*/
+self.__uv$config = {
+    prefix: '/service/',
+    bare: '/bare/',
+    encodeUrl: Ultraviolet.codec.plain.encode,
+    decodeUrl: Ultraviolet.codec.plain.decode,
+    handler: '/uv.handler.js',
+    client: '/uv.client.js',
+    bundle: '/uv.bundle.js',
+    config: '/uv.config.js',
+    sw: '/uv.sw.js',
+};
